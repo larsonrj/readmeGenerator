63f56094d58bcc5d405010adec213f260895caeb
Ryan <rylarson17@gmail.com>
2022-11-18 14:00:49 -0700

    Updated index.js to take answers from prompt and generate title for readme

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: "input",
+    message: "What is the title for your project?",
+    name: "title",
+  },
+];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      return generateMarkdown(answers);
+    })
+    .then(function (readmeText) {
+      fs.writeFile("readme.md", readmeText, (err) =>
+        err ? console.error(err) : console.log("Success!")
+      );
+    });
+}
 
 // Function call to initialize app
 init();
